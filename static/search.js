385bc975a683e691874b44cec778a3ee48d11358
agabani <agabani@users.noreply.github.com>
2020-11-12 19:57:11 +0000

    feat: static search box

---
+++ static/search.js
@@ -0,0 +1,12 @@
+var content = [
+    { title: 'Andorra' },
+    { title: 'United Arab Emirates' },
+    { title: 'Afghanistan' },
+    { title: 'Antigua' },
+    { title: 'Anguilla' },
+    { title: 'Albania' },
+]
+
+$('.ui.search').search({
+    source: content
+});
