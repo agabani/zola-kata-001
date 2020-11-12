724c4de7fd9e4bb773ebaa3747f62b0c0d906e0c
agabani <agabani@users.noreply.github.com>
2020-11-12 21:16:02 +0000

    feat: search integration

--- static/search.js
+++ static/search.js
@@ -1,12 +1,43 @@
-var content = [
-    { title: 'Andorra' },
-    { title: 'United Arab Emirates' },
-    { title: 'Afghanistan' },
-    { title: 'Antigua' },
-    { title: 'Anguilla' },
-    { title: 'Albania' },
-]
+zola = {};
+zola.search = {};
+zola.search.ready = function() {
 
-$('.ui.search').search({
-    source: content
-});
+    var $search = $('.ui.search');
+
+    var index = elasticlunr.Index.load(window.searchIndex);
+
+    $search.search({
+        type: 'category',
+        apiSettings: {
+            responseAsync: function(settings, callback){
+                var response = {
+                    success: true,
+                    results : {}
+                };
+
+                let results = index.search(settings.urlData.query);
+
+                $.each(results, function(index, item) {
+                    var category = 'AMD' || 'Unknown';
+
+                    if (response.results[category] === undefined) {
+                        response.results[category] = {
+                            name: category,
+                            results: []
+                        }
+                    }
+
+                    response.results[category].results.push({
+                        title: item.doc.title,
+                        description: item.doc.body,
+                        url: item.ref
+                    });
+                });
+
+                callback(response);
+            }
+        }
+    });
+}
+
+$(document).ready(zola.search.ready);
